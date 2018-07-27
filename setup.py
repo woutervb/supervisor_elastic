602e6052213e1297c0da08959c4179001448a8fe
Wouter van Bommel <wvanbommel@proigia.nl>
2018-07-27 13:55:57 +0200

    Include dependencies in sdist package

--- setup.py
+++ setup.py
@@ -5,21 +5,14 @@ test_requirements = open('test-requirements.txt').read().splitlines()
 with open('requirements.txt') as requirements:
     setup(
         name='supervisor-elastic',
-        version='0.0.1',
+        version='0.0.2',
         description='Stream supervisord logs to elastic via redis',
         author='Wouter van Bommel',
         author_email='wouter@vanbommelonline.nl',
-        url='https://github.com/woutervb/supervisor-elastic',
+        url='https://github.com/woutervb/supervisor_elastic',
         license='GPLv2',
         long_description=open('README.md').read(),
         packages=find_packages(exclude=['tests']),
-        package_data={
-            'supervisor-elastic': [
-                'README.md',
-                'requirements.txt',
-                'test-requirements.txt',
-            ],
-        },
         entry_points={
             'console_scripts': [
                 'supervisor_elastic = supervisor_elastic:main',
